831cbd3d7091860416b94ee933fb0a15f62b9b18
Surya Teja <45379824+suryatejaj97@users.noreply.github.com>
2021-07-27 17:25:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import HomeComponent from "./pages/HomeComponent";
+import AccountComponent from "./pages/AccountComponent";
+import CardComponent from "./pages/CardComponent";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="ui container">
+      <h2>Protected Routes</h2>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomeComponent} />
+          <Route path="/cards" component={CardComponent} />
+          <Route path="/accounts" component={AccountComponent} />
+        </Switch>
+      </Router>
     </div>
   );
 }
